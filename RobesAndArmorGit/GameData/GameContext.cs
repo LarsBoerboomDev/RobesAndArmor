54f5bc68629373965933a936fa90bc914f11d970
crazyavak <lars.boerboom@outlook.com>
2018-02-26 11:00:34 +0100

    - project setup

---
+++ RobesAndArmorGit/GameData/GameContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameData
+{
+    public class GameContext : DbContext
+    {
+        public GameContext(DbContextOptions options) : base(options) { }
+
+
+        /*
+        public DbSet<Models.Type> types { get; set; }
+        public DbSet<Enemy> enemies { get; set; }
+        public DbSet<News> newsLetter { get; set; }
+        */
+
+    }
+}
